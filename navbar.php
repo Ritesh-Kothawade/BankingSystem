9cf397b338c81776ec37c269cc9271179721aa29
Ritesh Kothawade <riteshbkothawade@gmail.com>
2022-02-20 14:53:57 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,19 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #4040a1;"><b>VIGHNAHARTA BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #4040a1;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="customerlist.php" style="color : #4040a1;"><b>Customer List</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #4040a1;"><b>Transaction History</b></a>
+              </li>
+          </div>
+      </nav>
